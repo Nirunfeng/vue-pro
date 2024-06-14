c546772736c62cf779718a6f47624fd36e851adb
nirunfeng <nirunfeng@hikvision.com.cn>
2024-06-14 14:01:40 +0800

    提交注册、登录、忘记密码静态页面

--- App.vue
+++ App.vue
@@ -1,32 +1,56 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+import Vue from 'vue'
+
+export default {
+  created() {
+    // #ifdef APP-PLUS
+    //关闭程序启动界面
+    plus.navigator.closeSplashscreen();
+    // #endif
+  },
+  //生命周期函数，应用启动执行一次
+  onLaunch: function() {
+
+    console.log('App Launch')
+    //适配环境
+    uni.getSystemInfo({
+      success: function(e) {
+        // #ifndef MP
+        Vue.prototype.StatusBar = e.statusBarHeight;
+        if (e.platform == 'android') {
+          Vue.prototype.CustomBar = e.statusBarHeight + 50;
+        } else {
+          Vue.prototype.CustomBar = e.statusBarHeight + 45;
+        };
+        // #endif
+
+        // #ifdef MP-WEIXIN
+        Vue.prototype.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        Vue.prototype.Custom = custom;
+        Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+        // #endif
+
+        // #ifdef MP-ALIPAY
+        Vue.prototype.StatusBar = e.statusBarHeight;
+        Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
+        // #endif
+      }
+    })
+  },
+  onShow: function() {
+    console.log('App 开启')
+  },
+  onHide: function() {
+    console.log('App 关闭')
+  }
+}
 </script>
 
-<style lang="scss">
-	/*每个页面公共css */
-	@import '@/uni_modules/uni-scss/index.scss';
-	/* #ifndef APP-NVUE */
-	@import '@/static/customicons.css';
-	// 设置整个项目的背景色
-	page {
-		background-color: #f5f5f5;
-	}
+<style >
 
-	/* #endif */
-	.example-info {
-		font-size: 14px;
-		color: #333;
-		padding: 10px;
-	}
+body{
+  background: #FFFFFF !important;
+}
+
 </style>
