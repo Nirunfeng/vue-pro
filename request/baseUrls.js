b76ac77f8c153dad2c2a5052da14f94d82482791
Nirunfeng <2570034697@qq.com>
2024-11-17 16:23:24 +0800

    更改url地址

--- request/baseUrls.js
+++ request/baseUrls.js
@@ -1,5 +1,5 @@
 const baseUrls = {
-    base1: 'http://127.0.0.1:7120/service-user',
-    base2: 'http://127.0.0.1:7200/service-dictionary'
+    base1: 'http://192.168.3.42:7120/service-user',
+    base2: 'http://192.168.3.42:7200/service-dictionary'
 }
 export default baseUrls
